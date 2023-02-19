fbc95d83f75cc9b47c07046da7648f7ea73539f7
Кренделев Роман <romanwebdev71@gmail.com>
2023-02-19 23:23:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -10,16 +10,23 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
-app.use(express.json())
-app.use('/auth', authRouter)
-app.use('/products', productRouter)
-app.use('/orders', orderRouter)
+// app.use(express.json())
+// app.use('/auth', authRouter)
+// app.use('/products', productRouter)
+// app.use('/orders', orderRouter)
+
+app.get('/test', async (req, res) => {
+    return res.send({
+        a: '1',
+        b: '10'
+    });
+})
 
 
 
 const start = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://roman:roman123@cluster0.uq96p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+        // await mongoose.connect(`mongodb+srv://roman:roman123@cluster0.uq96p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (error) {
         console.log(error)
